6115da57fdedb19c102636ba0ef18609feba5cf9
AmdHamdani <Hamdaniryuuken@gmail.com>
2020-07-19 14:33:54 +0700

    Change string type to ValueType

--- Assets/NoodleEater/Data Editor/Editor/Data/FieldValue.cs
+++ Assets/NoodleEater/Data Editor/Editor/Data/FieldValue.cs
@@ -1,7 +1,9 @@
 namespace NoodleEater.DataEditor.Data {
+
+    [System.Serializable]
     public class FieldValue {
         public string fieldName;
-        public string type;
+        public ValueType type;
         public string value;
 
         public override string ToString() {
