a82b7c7a3158db0ce5297137ff2144f42acdc329
AmdHamdani <Hamdaniryuuken@gmail.com>
2020-07-19 12:55:52 +0700

    Add template script

---
+++ Assets/NoodleEater/Data Editor/Editor/Data/Constant.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace NoodleEater.DataEditor.Data {
+    public class Constant {
+        
+        public static string SCRIPTPATH = Application.dataPath + "/Scripts/";
+
+    }
+}
