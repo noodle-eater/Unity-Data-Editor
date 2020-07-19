d45d9bd6519925ac7660309630bbcdea969e9cee
AmdHamdani <Hamdaniryuuken@gmail.com>
2020-07-19 08:18:49 +0700

    Add DataEditorWindow

---
+++ Assets/Data Editor/Editor/DataEditorWindow.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class DataEditorWindow : EditorWindow {
+
+    [MenuItem("Unity-Data-Editor/DataWindow")]
+    private static void ShowWindow() {
+        var window = GetWindow<DataEditorWindow>();
+        window.titleContent = new GUIContent("DataWindow");
+        window.Show();
+    }
+
+    private void OnGUI() {
+        
+    }
+}
