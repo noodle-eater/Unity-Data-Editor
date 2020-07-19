52e361067062e56b08b3c1a6f7a8f6a1cad2c721
AmdHamdani <Hamdaniryuuken@gmail.com>
2020-07-19 09:17:02 +0700

    Add WindowUIDrawer

---
+++ Assets/NoodleEater/Data Editor/Editor/Window/WindowUIDrawer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace NoodleEater.DataEditor
+{
+    public class WindowUIDrawer
+    {
+
+        public void DrawHorizontalLabel(params string[] labels)
+        {
+            EditorGUILayout.BeginHorizontal();
+            for (int i = 0; i < labels.Length; i++)
+            {
+                GUILayout.FlexibleSpace();
+                GUILayout.Label(labels[i]);
+                GUILayout.FlexibleSpace();
+            }
+            EditorGUILayout.EndHorizontal();
+        }
+
+        public void DrawButton(string buttonName, System.Action OnClick) {
+            if(GUILayout.Button(buttonName)) {
+                OnClick?.Invoke();
+            }
+        }
+    }
+}
