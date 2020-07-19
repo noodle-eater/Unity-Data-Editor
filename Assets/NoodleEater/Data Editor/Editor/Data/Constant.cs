d7a58b98d1391e413f8cb19e0736e3e34a524b88
AmdHamdani <Hamdaniryuuken@gmail.com>
2020-07-19 21:57:20 +0700

    Add template & cache path to constants

--- Assets/NoodleEater/Data Editor/Editor/Data/Constant.cs
+++ Assets/NoodleEater/Data Editor/Editor/Data/Constant.cs
@@ -4,6 +4,14 @@ namespace NoodleEater.DataEditor.Data {
     public class Constant {
         
         public static string SCRIPTPATH = Application.dataPath + "/Scripts/";
+        public static string PROJECTHPATH = Application.dataPath.Substring(0, Application.dataPath.Length - 6);
+        public static string DATACACHE = PROJECTHPATH + "/Temp/NoodleEater/Data";
+        public static string GENERATEDPATH = SCRIPTPATH + "/Generated/";
+        public static string TEMPLATEPATH = Application.dataPath + "/NoodleEater/Data Editor/Editor/Template/";
 
+        public static string JSONCACHEPATH = DATACACHE + "/cache.json";
+        public static string CLASSTEMPLATE = TEMPLATEPATH + "/class.txt";
+        public static string FIELDTEMPLATE = TEMPLATEPATH + "/field.txt";
+        public static string NAMESPACETEMPLATE = TEMPLATEPATH + "/namespace.txt";
     }
 }
