f13267d16a4914798847744136e5172e52732d4c
AmdHamdani <Hamdaniryuuken@gmail.com>
2020-07-20 23:25:15 +0700

    Add class generator to code controller

--- Assets/NoodleEater/Data Editor/Editor/Controller/CodeGeneratorController.cs
+++ Assets/NoodleEater/Data Editor/Editor/Controller/CodeGeneratorController.cs
@@ -1,18 +1,29 @@
 using System.Collections.Generic;
 using System.IO;
 using NoodleEater.DataEditor.Data;
+using NoodleEater.DataEditor.Generator;
 using UnityEditor;
 
 namespace NoodleEater.DataEditor.Controller {
 
     public class CodeGeneratorController {
         
+        private ClassGenerator _classGenerator = new ClassGenerator();
+
+        public void Init() {
+            _classGenerator.Init();
+        }
+
         public void CreateJson(FieldValueContainer container) {
-            string jsonPath = Constant.GENERATEDDATAPATH + $"/{container.className}.json";
+            string jsonPath = Constant.GENERATEDDATAPATH + $"/{container.className.ToLower()}.json";
             FileHelper.CreateDirectory(Constant.GENERATEDDATAPATH);
             FileHelper.CreateFile(jsonPath);
             File.WriteAllText(jsonPath, container.ToJson());
             AssetDatabase.Refresh();
+        }
+
+        public void GenerateClass(FieldValueContainer container) {
+            _classGenerator.GenerateClass(container);
         }
 
     }
