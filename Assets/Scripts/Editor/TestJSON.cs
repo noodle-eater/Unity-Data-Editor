95415e606be070145fcdc834a02e72edb9993b6a
AmdHamdani <Hamdaniryuuken@gmail.com>
2020-07-21 21:19:51 +0700

    Add Test

---
+++ Assets/Scripts/Editor/TestJSON.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class TestJSON 
+{
+
+    [MenuItem("Test/Json")]
+    public static void TestJson() {
+        var result = JsonUtility.FromJson<Test>(File.ReadAllText(Application.dataPath + "/Resources/Generated/test.json"));
+        Debug.Log(result.wew);
+    }
+
+}
---
+++ Assets/Scripts/Generated/Test.cs
@@ -0,0 +1,6 @@
+[System.Serializable]
+public class Test {
+
+	public int wew;
+
+}
